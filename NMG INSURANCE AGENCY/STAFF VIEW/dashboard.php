51addd77cad1fbc7aca2c3d733a0becb5db3ccac
RuiRui26 <rilgdinampo26@yahoo.com>
2025-03-08 02:24:29 +0800

    Revert "Merge branch 'main' of https://github.com/RuiRui26/SBDE-CO"
    This reverts commit 14118d242edf764a4c4de325191a778f20ec5ea9, reversing
    changes made to 75816a8926bcc5efd49e22cd21ca1b70d85d47b2.

--- NMG INSURANCE AGENCY/STAFF VIEW/dashboard.php
+++ NMG INSURANCE AGENCY/STAFF VIEW/dashboard.php
@@ -11,17 +11,19 @@
 </head>
 
 <body>
+    <!-- Sidebar -->
     <div class="sidebar">
         <img src="img5/logo.png" alt="Logo" class="logo">
         <ul class="menu">
             <li><a href="dashboard.php"><img src="img5/dashboard.png" alt="Dashboard Icon"> Dashboard</a></li>
-            <li><a href="staff.php"><img src="img5/adminprofile.png" alt="Admin Icon"> Staff Information</a></li>
+            <li><a href="staff_info.php"><img src="img5/adminprofile.png" alt="Admin Icon"> Staff Information</a></li>
             <li><a href="customer.php"><img src="img5/customers.png" alt="Customers Icon"> Customers</a></li>
             <li><a href="search.php"><img src="img5/search.png" alt="Search Icon"> Search Policy</a></li>
             <li><a href="../../Logout_Login/Logout.php"><img src="img5/logout.png" alt="Logout Icon"> Logout</a></li>
         </ul>
     </div>
 
+    <!-- Admin Profile Dropdown -->
     <div class="profile-dropdown">
         <img src="img5/samplepic.png" alt="Admin Avatar" class="avatar" onclick="toggleProfileMenu()">
         <div class="profile-menu" id="profileMenu">
@@ -32,13 +34,16 @@
         </div>
     </div>
 
+    <!-- Real-time Date and Time -->
     <div class="datetime-display" id="datetimeDisplay"></div>
 
+    <!-- Main Content -->
     <div class="main-content">
         <div class="welcome-container">
             <h1>Welcome, Staff</h1>
         </div>
 
+        <!-- Stats Container -->
         <div class="stats-container" id="stats-container">
             <?php
             $stats = [
@@ -70,48 +75,6 @@
             }
             ?>
         </div>
-
-        <div class="leaderboard-container">
-            <h2>Staff Leaderboard (Top Sales)</h2>
-            <div class="leaderboard-table-wrapper">
-                <table class="leaderboard-table">
-                    <thead>
-                        <tr>
-                            <th>Rank</th>
-                            <th>Picture</th>
-                            <th>Staff Name</th>
-                            <th>Total Sales</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $staffSales = [
-                            ["name" => "Robby Patrick Enriquez", "sales" => 350, "picture" => "img5/samplepic.png"],
-                            ["name" => "Andy Rilg Dinampo", "sales" => 300, "picture" => "img5/samplepic.png"],
-                            ["name" => "Alekxiz Solis", "sales" => 275, "picture" => "img5/samplepic.png"],
-                            ["name" => "John Mchales Buenaventura", "sales" => 250, "picture" => "img5/samplepic.png"],
-                            ["name" => "Charlie White", "sales" => 220, "picture" => "img5/samplepic.png"],
-                            ["name" => "Eve Green", "sales" => 200, "picture" => "img5/samplepic.png"],
-                            ["name" => "Frank Black", "sales" => 180, "picture" => "img5/samplepic.png"]
-                        ];
-
-                        usort($staffSales, function ($a, $b) {
-                            return $b['sales'] - $a['sales'];
-                        });
-
-                        foreach ($staffSales as $index => $staff) {
-                            echo "<tr>
-                                    <td>" . ($index + 1) . "</td>
-                                    <td><img src='{$staff['picture']}' alt='{$staff['name']}' class='staff-picture'></td>
-                                    <td>{$staff['name']}</td>
-                                    <td>{$staff['sales']}</td>
-                                </tr>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
     </div>
 
     <script>
@@ -129,9 +92,9 @@
             const transactionNumber = card.querySelector('.transaction-number');
 
             if (select.value === 'default') {
-                transactionNumber.textContent = transactionNumber.dataset.default;
+                transactionNumber.textContent = transactionNumber.dataset.default; // Reset to default value
             } else {
-                transactionNumber.textContent = select.value;
+                transactionNumber.textContent = select.value; // Set selected value
             }
         }
 
