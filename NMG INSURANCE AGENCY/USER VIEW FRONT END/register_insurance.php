c87a4751728ec3cacfeec760ea5925a9913313c3
RuiRui26 <rilgdinampo26@yahoo.com>
2025-03-10 00:43:45 +0800

    push????????

--- NMG INSURANCE AGENCY/USER VIEW FRONT END/register_insurance.php
+++ NMG INSURANCE AGENCY/USER VIEW FRONT END/register_insurance.php
@@ -16,20 +16,8 @@
     <div class="container">
         <h2>Register Your Insurance</h2>
         
-        <form action="register_insurance.html" method="POST" enctype="multipart/form-data">
+        <form action="process_insurance.php" method="POST" enctype="multipart/form-data">
             
-            <label for="name">Full Name:</label>
-            <input type="text" id="name" name="name" required>
-
-            <label for="address">Address:</label>
-            <input type="text" id="address" name="address" required>
-
-            <label for="mobile">Mobile Number:</label>
-            <input type="tel" id="mobile" name="mobile" required pattern="[0-9]{11}" placeholder="Enter your 11-digit mobile number" 
-            oninvalid="this.setCustomValidity('Please enter exactly 11 digits.')"
-            oninput="this.setCustomValidity('')">
-
-
             <label for="plate_number">Plate Number:</label>
             <input type="text" id="plate_number" name="plate_number" required>
 
@@ -38,11 +26,11 @@
 
             <label for="insurance_type">Type of Insurance:</label>
             <select id="insurance_type" name="insurance_type" required>
-            <option value="">Select Insurance Type</option>
-            <option value="tpl">Third Party Liability (TPL) Insurance</option>
-            <option value="tppd">Third Party Property Damages (TPPD) Insurance</option>
-            <option value="od">Own Damage (OD) Insurance</option>
-            <option value="upa">Unnamed Personal Accident(UPA) Insurance</option>
+                <option value="">Select Insurance Type</option>
+                <option value="tpl">Third Party Liability (TPL) Insurance</option>
+                <option value="tppd">Third Party Property Damages (TPPD) Insurance</option>
+                <option value="od">Own Damage (OD) Insurance</option>
+                <option value="upa">Unnamed Personal Accident (UPA) Insurance</option>
             </select>
 
             <label for="or_picture">Upload OR Picture:</label>
@@ -56,12 +44,12 @@
     </div>
 
     <!--------------- Footer ------------------>
-	<footer>
-		<div class="footer-container">
-			<p class="para-line white">Copyright NMG Insurance Agency ©2025</p>
-		</div>
-	</footer>
-	<!--------------- Footer ------------------>
+    <footer>
+        <div class="footer-container">
+            <p class="para-line white">Copyright NMG Insurance Agency ©2025</p>
+        </div>
+    </footer>
+    <!--------------- Footer ------------------>
 
 </body>
 </html>
